314753e9cc7d41141b011096245989943293056c
Alexey Demyanov <deal110988@him-mail.ch>
2025-03-17 12:39:37 +0100

    managing to extract all numbers from appIds

--- src/main.ts
+++ src/main.ts
@@ -10,10 +10,18 @@ testFunc();
 const fileReader = new FileReader('logs/appids.txt');
 fileReader.read();
 const logText = fileReader.toString();
-// const pattern = /\\"appid\\":/g;
 const pattern = /\\"appid\\":\d{1,100},/g;
+const numberPattern = /\d{1,100}/g;
 
 const foundItems = logText.match(pattern);
-// console.log(logText)
-// console.log(foundItems);
 console.log(foundItems?.length);
+
+if (foundItems) {
+  console.log(foundItems[0]);
+  console.log(foundItems[1]);
+  console.log(foundItems[2]);
+
+  for (let i = 0; i < 10; i++) {
+    console.log(foundItems[i].match(numberPattern));
+  }
+}
