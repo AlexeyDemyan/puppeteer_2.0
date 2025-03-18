026a194629c02762797bb40286257c56d0663466
Alexey Demyanov <deal110988@him-mail.ch>
2025-03-18 15:57:52 +0100

    finished script to prase principal data

--- test.js
+++ test.js
@@ -1,9 +1,42 @@
-const logTextPart =
-    'items\":[{\"appid\":242550,\"priority\":1,\"date_added\":1375550321},{\"appid\":2390,\"priority\":2,\"date_added\":1375552201},{\"appid\":2600,\"priority\":3,\"date_added\":1384417385},{\"appid\":450';
+import puppeteer from "puppeteer";
+import { config } from "dotenv";
 
-const regex = /\"appid\":\d{1,100},/g;
+const puppeteerInit = async () => {
+    console.log("running puppeteer");
+    const parsedConfig = config();
+    if (parsedConfig.error) {
+        throw new Error("Can't read .env file. Perhaps the file does not exist.");
+    }
 
-const found = logTextPart.match(regex)
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-// console.log(pattern.exec(logTextPart));
-console.log(found);
+    await page.goto('file:///C:/Users/ademyanov/Desktop/Codes_and_Repos/puppeteer_2.0/logs/scraped_page.html');
+
+    //const appNameElement = await page.locator(`#${process.env.NAME_ID}`).waitHandle();
+    //const appImageElement = await page.locator(`.${process.env.HEADER_IMAGE_CLASS}`).waitHandle();
+
+    // const appName = await appNameElement?.evaluate((el) => el.textContent);
+    // const appImage = await appImageElement?.evaluate((el) => el.getAttribute('src'));
+
+    // const descriptionElement = await page.locator(`.${process.env.DESCRIPTION_CLASS}`).waitHandle();
+    // const description = await descriptionElement?.evaluate((el) => el.textContent);
+    // console.log(description.trim());
+
+    //working solution, need to evaluate in real time how the texts will come out
+    const tags = await page.evaluate(() => {
+        let data = [];
+        let elements = document.getElementsByClassName('app_tag');
+        for (let element of elements) {
+            console.log(element.textContent);
+            data.push(element.textContent);
+        }
+        return data;
+    })
+
+    console.log(tags);
+
+    await browser.close();
+}
+
+puppeteerInit();
